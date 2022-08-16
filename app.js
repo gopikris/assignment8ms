dbdd3315c4e6325f13e79ba4f5b9fe44f93ca9a5
Gopi V <gvaradar@in.ibm.com>
2022-08-17 00:01:02 +0530

    minor change to app.js

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
+  // render the error page 
+  res.status(err.status || 501);
   res.render('error');
 });
 
