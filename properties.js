015bb6d91f1d8064c80933345c9a36a773493a20
Gopi V <gvaradar@in.ibm.com>
2022-08-16 22:57:43 +0530

    Updated my DB

--- properties.js
+++ properties.js
@@ -1,4 +1,5 @@
 module.exports = {
     // DB_URL: "mongodb://localhost:27017/restaurantsDB"
-    DB_URL: "mongodb+srv://admin:admin@cluster0.h7twt.mongodb.net/?retryWrites=true&w=majority"
+    // DB_URL: "mongodb+srv://admin:admin@cluster0.h7twt.mongodb.net/?retryWrites=true&w=majority"
+    DB_URL: "mongodb+srv://admin:admin@restaurant-db.z8kk6.mongodb.net/?retryWrites=true&w=majority"
 }
